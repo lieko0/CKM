302eca44b8a7709b6bbedab84a08a73cdaeeb95e
katfr <katarinalydia@hotmail.com>
2022-04-17 01:00:02 -0300

    Adição de consultar todos os membros

---
+++ src/Controller/ConsultarTodosMembros.php
@@ -0,0 +1,67 @@
+<?php
+include_once "..\Persistence\Connection.php";
+include_once "..\Persistence\MembroDAO.php";
+
+$conexao = new Connection();
+$conexao = $conexao->getConnection();
+
+$membroDao = new MembroDAO();
+$res = $membroDao->consultarTodosMembros($conexao);
+
+if($res->num_rows > 0) {
+
+    echo "
+    <!DOCTYPE html>
+            <html>
+            <head>
+            <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+            </style>
+            </head>
+            <body>
+            
+            <div class='topnav'>
+                <a class='active' href='..\index.html'>Início</a>
+                <a href='..\View\Membro.html'>Voltar</a>
+            </div>
+
+            <h2>Membros</h2><table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Email</th>
+            </tr>";
+
+    while ($registro = $res->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$registro['Id']."</td>".
+             "<td>".$registro['Nome']."</td>". // O campo de registro deve ser igual ao do BD
+             "<td>".$registro['Cpf']."</td>".
+             "<td>".$registro['Email']."</td>";
+        echo "</tr>";
+
+    }
+    echo "</table></body></html>";
+} else {
+    echo "<script>
+    alert('Não há membros cadastrados');
+    location.href ='../View/Membro.html';
+    </script>";
+}
+
+?>
